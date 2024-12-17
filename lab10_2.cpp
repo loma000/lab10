e56b1ea97126c3af4bd070bb91fa445e8612e85c
loma000 <dechartorn.som@gmail.com>
2024-12-17 16:39:40 +0700

    f

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,18 +1,24 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main (){
+int main()
+{
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string text;
+	dest << "-------------------- BOOM ---------------------" << endl;
+	while (getline(source, text))
+	{
 
-    source.close();
-    dest.close();
+		dest << text << endl;
+	}
+
+	dest << "-------------------- HA!! ---------------------";
+	source.close();
+	dest.close();
 	return 0;
 }
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<string>
+#include<math.h>
+using namespace std;
+
+int main(){
+    	ifstream source;
+ string score;
+ double scorenum = 0,scoresd = 0;
+ int i=0;
+	source.open("score.txt");
+ while(getline(source,score)){
+     
+     scorenum += stod(score);
+     scoresd += pow(stod(score),2);
+     
+     i++;
+     
+     
+ }
+ 
+ 
+ 
+    cout << "Number of data = "<<i<<endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< scorenum/i<<endl;
+    cout << "Standard deviation = "<<sqrt( (scoresd  /i) -pow(scorenum/i,2) )<<endl;
+}
